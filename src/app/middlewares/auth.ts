29d88f2079065a55c552cef178f788bf9dde5358
abdul hannan <shohas563@gmail.com>
2024-06-22 09:11:38 -0400

    vercel add

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -6,11 +6,6 @@ import httpStatus from 'http-status';
 import config from '../config';
 import { TUserRole } from '../modules/user/user.interface';
 
-// interface AuthenticatedRequest extends Request {
-//   userId?: string;
-//   user?: JwtPayload;
-// }
-
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
@@ -49,46 +44,3 @@ const auth = (...requiredRoles: TUserRole[]) => {
 };
 
 export default auth;
-
-// import jwt, { JwtPayload } from 'jsonwebtoken';
-// import { NextFunction, Request, Response } from 'express';
-// import catchAsync from '../utils/catchAsync';
-// import AppError from '../errors/AppError';
-// import httpStatus from 'http-status';
-// import config from '../config';
-// import { TUserRole } from '../modules/user/user.interface';
-
-// const auth = (...requiredRoles: TUserRole[]) => {
-//   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-//     const authHeader = req.headers.authorization;
-
-//     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-//       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
-//     }
-
-//     const token = authHeader.split(' ')[1];
-
-//     jwt.verify(
-//       token,
-//       config.jwt_access_secret as string,
-//       function (err, decoded) {
-//         if (err) {
-//           throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
-//         }
-//         const role = (decoded as JwtPayload).role;
-
-//         if (requiredRoles && !requiredRoles.includes(role)) {
-//           throw new AppError(
-//             httpStatus.UNAUTHORIZED,
-//             'You are not authorized !',
-//           );
-//         }
-
-//         req.user = decoded as JwtPayload;
-//         next();
-//       },
-//     );
-//   });
-// };
-
-// export default auth;
